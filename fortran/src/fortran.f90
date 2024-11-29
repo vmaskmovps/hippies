30974f00331dbe5171e079fbddcb5fa61b21e983
Ștefan-Iulian Alecu <165364995+vmaskmovps@users.noreply.github.com>
2024-11-29 20:51:43 +0200

    build: add functional_fortran

--- fortran/src/fortran.f90
+++ fortran/src/fortran.f90
@@ -1,10 +1,22 @@
 module knight
+   use functional
    implicit none
    private
 
-   public :: say_hello
+   public :: say_hello, fibonacci
 contains
+   pure recursive integer function fibonacci(n) result(fib)
+      integer,intent(in) :: n
+      if (n == 0) then
+         fib = 0
+      else if (n == 1) then
+         fib = 1
+      else
+         fib = fibonacci(n - 1) + fibonacci(n - 2)
+      end if
+   end function fibonacci
+
    subroutine say_hello
-      print *, "Hello, knight!"
+      print *, map(fibonacci, [17, 5, 13, 22])
    end subroutine say_hello
 end module knight
